7fade2c23afc35f701209a55332338f936d756e1
Josué <josueenrique1999@gmail.com>
2020-11-29 12:50:39 -0600

    Changed reproduction method
    This is a functional version of the bot;
    
    - The bot enters the voice channel once a user disconnects from it and play the video.
    - The bot can handle basic video reproduction.
    
    In order to get working the bot video reproduction, the  following packages are required:
    - ytdl-core
    - opus
    - ffmpeg in server
    
    check: 
    https://discordjs.guide/popular-topics/faq.html#how-do-i-play-music-from-youtube
    
    for more information about installing these packages

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
+//for videos
+const ytdl = require('ytdl-core');
 
 client.login(process.env.BOT_TOKEN);
 
@@ -11,12 +13,32 @@ client.once('ready', () => {
 
 
 client.on('voiceStateUpdate', (oldState, newState) => {
-    let oldChannel = oldState.member.voice.channelID;
-    let newChannel = newState.member.voice.channelID;
+    let oldChannel = oldState.channel;
+    let newChannel = newState.channel;
+    let isBot = oldState.member.user.bot;
+    let voiceChannel = client.channels.cache.get("398167945248899088"); //default voicheChannel to play
 
-    let channel = client.channels.cache.get('398167945248899086');
-
-    if(oldChannel === null && newChannel === null){
-        channel.send("-play https://www.youtube.com/watch?v=b8PxzPxI8Os");
+    //Get the users current voiceChannel
+    if(oldChannel !== null){
+        voiceChannel = client.channels.cache.get(oldState.channelID);
+    }else if(newChannel !== null){
+        voiceChannel = client.channels.cache.get(newChannel.channelID);
     }
+
+    //Check if a user disconects from voice channel
+    if(oldChannel !== null && newChannel === null && isBot === false){
+        voiceChannel.join()
+            .then(connection => {
+                const stream = ytdl('https://www.youtube.com/watch?v=b8PxzPxI8Os', { filter: 'audioonly' });
+                const dispatcher = connection.play(stream);
+                //on finish()
+                dispatcher.on('finish', () => voiceChannel.leave());
+            })
+            .catch(console.error);
+    }
+
+    //debug stuff
+    console.log("-----------------");
+    console.log(voiceChannel);
+    console.log("-----------------");
 });
